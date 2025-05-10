606b7fab9943af2f08f55b0ac68d36b17272ec1a
AniketJoshi-ready <janiketv2002@gmail.com>
2025-05-10 12:35:57 +0530

    Delete assignment1.8.py

--- assignment1.8.py
+++
@@ -1,8 +0,0 @@
-def main():
-    n=int(input("ho much times * want to display tell me number darling : "))
-    for i in range(n,0,-1):
-      print("*",end=" ")
-
-if __name__=="__main__":
-   main()
-    
